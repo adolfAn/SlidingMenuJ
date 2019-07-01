13971707c0e3913a1722cd6247121666b0614bd1
anhuiqing <anhuiqing@bigbaser.com>
2019-07-02 00:47:16 +0800

    修改 slidingmenu

--- SlidingMenuDemo/mainActivity/src/main/java/com/anhq/slidemenu/activity/MainActivity.java
+++ SlidingMenuDemo/mainActivity/src/main/java/com/anhq/slidemenu/activity/MainActivity.java
@@ -80,17 +80,17 @@ public class MainActivity extends SlidingFragmentActivity implements OnClickList
 		mSlidingMenu.setBehindCanvasTransformer(new SlidingMenu.CanvasTransformer() {
 			@Override
 			public void transformCanvas(Canvas canvas, float percentOpen) {
-				float scale = (float)(percentOpen * 0.25+0.75);
-				canvas.scale(scale, scale, - canvas.getWidth() / 2,
-						canvas.getHeight() / 2);
+//				float scale = (float)(percentOpen * 0.25+0.75);
+//				canvas.scale(scale, scale, - canvas.getWidth() / 2,
+//						canvas.getHeight() / 2);
 			}
 		});
 
 		mSlidingMenu.setAboveCanvasTransformer(new SlidingMenu.CanvasTransformer() {
 			@Override
 			public void transformCanvas(Canvas canvas, float percentOpen) {
-				float scale = (float) (1 - percentOpen * 0.25);
-				canvas.scale(scale, scale, 0, canvas.getHeight() / 2);
+//				float scale = (float) (1 - percentOpen * 0.25);
+//				canvas.scale(scale, scale, 0, canvas.getHeight() / 2);
 			}
 		});
 	}
